4a0bb803fe818e03bd3e3f98dbb9a5ede6631437
Russ-S <rstanleym74@gmail.com>
2023-03-13 16:38:21 +0000

    Create validate_data function with try / except statement and test

--- run.py
+++ run.py
@@ -12,14 +12,34 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
+
 def get_sales_data():
-    """.git/Get sales figures input from the user
+    """
+    Get sales figures input from the user
     """
     print("Please enter sales data from the last market")
     print("Data should be 6 numbers, seperated by commas.")
     print("Example: 10,11,12,16,19,5\n")
 
     data_str = input("Enter your data here:")
-    print(f"The data provided is: {data_str}")
+
+    sales_data = data_str.split(",")
+    validate_data(sales_data)
+
+def validate_data(values):
+    """
+    inside the try / except statement -  converts all string values to integers, raises a ValueError if strings can't be converted to int or if there is not exactly 6 values
+    """
+    try:
+        if len(values) != 6:
+            raise ValueError(
+                f"Exactly 6 values are required, you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}. Please try again.\n")
+
+
+
+    
 
 get_sales_data()
